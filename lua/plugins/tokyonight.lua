79d73d5b47026f86ddb747ebd668d8076ecaeeac
Angju <107686204+digital-deceit@users.noreply.github.com>
2024-12-04 00:58:45 -0400

    Formatted file

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -12,7 +12,7 @@ return {
 			sidebars = "transparent",
 			floats = "transparent",
 		},
-		on_colors = function (colors)
+		on_colors = function(colors)
 			colors.bg_statusline = colors.none
 		end,
 		on_highlights = function(highlights, colors)
