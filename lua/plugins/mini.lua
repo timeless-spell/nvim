4ec677fe50a7c2b0801e2863381e7e3cefa693ba
Angju <154499065+timeless-spell@users.noreply.github.com>
2024-12-21 17:08:29 -0400

    Modified dashboard

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -49,59 +49,59 @@ return {
 		vim.cmd([[:hi MiniStarterHeader guifg=#87C144]])
 		local starter = require("mini.starter")
 		starter.setup({
-			header = require("config.ascii").kraken,
+			header = require("config.ascii").neovim2,
 			-- TODO: Add more items
 			items = {
 				{
 					name = "Load Session",
 					action = "lua require('resession').load()",
-					section = "Resession",
+					section = "\t\tResession",
 				},
 				{
 					name = "Global",
 					action = "FzfLua files cwd=$HOME",
-					section = "Files",
+					section = "\t\tFiles",
 				},
 				{
 					name = "Find Files",
 					action = 'Telescope find_files layout_config={"width":0.95,"preview_width":0.6,"height":0.95}',
-					section = "Files",
+					section = "\t\tFiles",
 				},
 				{
 					name = "File Browser",
 					action = 'Telescope file_browser file_browser previewer=true layout_strategy=bottom_pane layout_config={"height":0.85}',
-					section = "Files",
+					section = "\t\tFiles",
 				},
 				{
 					name = "Recent Files",
 					action = 'Telescope oldfiles previewer=false layout_strategy=vertical layout_config={"width":0.8,"prompt_position":"top"}',
-					section = "Files",
+					section = "\t\tFiles",
 				},
 				{
 					name = "Lazy",
 					action = "Lazy",
-					section = "Neovim",
+					section = "\t\tNeovim",
 				},
 				{
 					name = "Plugins",
 					action = "lua require('telescope').extensions.file_browser.file_browser({ path = '~/.config/nvim/lua/plugins',border = false,previewer = false,layout_strategy = 'center',layout_config = { height = 0.7 },})",
-					section = "Neovim",
+					section = "\t\tNeovim",
 				},
 				{
 					name = "Files",
 					action = 'Telescope find_files cwd=$HOME/.config/nvim layout_config={"width":0.95,"preview_width":0.6,"height":0.95}',
-					section = "Neovim",
+					section = "\t\tNeovim",
 				},
 				{
 					name = "Directory",
 					action = [[
 					lua require("telescope").extensions.file_browser.file_browser({path=vim.fn.stdpath("config"),layout_strategy="horizontal",layout_config={height=0.90,prompt_position="top",width=0.90,preview_width=0.65,},grouped=true,depth=2,hidden={file_browser=true,folder_browser=true,},respect_gitignore=false,previewer=true})
 					]],
-					section = "Neovim",
+					section = "\t\tNeovim",
 				},
 			},
 			content_hooks = {
-				starter.gen_hook.adding_bullet(),
+				starter.gen_hook.adding_bullet(""),
 				starter.gen_hook.aligning("center", "center"),
 			},
 			footer = os.date(),
