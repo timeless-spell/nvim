3d3532fdc1d98b8c14c22b73e542bf71cdcaaf03
Angju <154499065+timeless-spell@users.noreply.github.com>
2024-12-21 17:07:45 -0400

    Modified config

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -11,4 +11,33 @@ return {
 		},
 	},
 	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+	{ -- NOTE: optional cmp completion source for require statements and module annotations
+		"iguanacucumber/magazine.nvim",
+		name = "nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+	},
+
+	-- NOTE: optional blink completion source for require statements and module annotations
+	-- {
+	-- 	"saghen/blink.cmp",
+	-- 	opts = {
+	-- 		sources = {
+	-- 			-- add lazydev to your completion providers
+	-- 			completion = {
+	-- 				enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+	-- 			},
+	-- 			providers = {
+	-- 				-- dont show LuaLS require statements when lazydev has items
+	-- 				lsp = { fallback_for = { "lazydev" } },
+	-- 				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
