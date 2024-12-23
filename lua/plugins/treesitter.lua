72a58276c500d36796acf0498230f82bebf9866f
Angju <107686204+digital-deceit@users.noreply.github.com>
2024-12-23 12:28:53 -0400

    Fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,31 @@
-return { -- Highlight, edit, and navigate code
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	main = 'nvim-treesitter.configs',
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {},
+	},
 	opts = {
-		ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"fish",
+			"regex",
+		},
 		auto_install = true,
 		highlight = {
 			enable = true,
 		},
-		indent = { enable = true, disable = { 'ruby' } },
-	}
+		indent = { enable = true, disable = { "ruby" } },
+	},
 }
