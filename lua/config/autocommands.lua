9d7ff3150e2fbca5a92adb4332a41f1dabb0300f
Angju <107686204+digital-deceit@users.noreply.github.com>
2024-12-26 04:49:46 -0400

    Added autocmd for removing the Windows path inside Neovim

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -5,3 +5,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	desc = "Disable Windows Path",
+	group = vim.api.nvim_create_augroup("disable-windows-path", { clear = true }),
+	callback = function()
+		local initial_path = vim.fn.getenv("PATH")
+		local pattern_starts = string.find(initial_path, "/mnt")
+		local extracted_path = string.sub(initial_path, 1, pattern_starts - 2)
+		vim.fn.setenv("PATH", extracted_path)
+	end,
+})
