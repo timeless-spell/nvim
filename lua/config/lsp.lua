7ae1ee139607fa49c52c57d0abf76db06b790d9a
Angju <154499065+timeless-spell@users.noreply.github.com>
2025-11-26 09:41:48 -0400

    Added typescript and javascript lsp settings.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,30 @@
+local ts_and_js_settings = {
+    typescript = {
+        inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+        },
+    },
+    javascript = {
+        inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+        },
+    },
+}
+
 local capabilities = {}
 
 local ok, blink = pcall(require, "blink.cmp")
@@ -29,21 +56,38 @@ local servers = {
             },
         },
     },
-    denols = {
-        root_dir = vim.fs.root(0, { "deno.json", "deno.jsonc" }),
+    ts_ls = {
+        root_dir = function (bufnr, on_dir)
+            local root_markers = { "tsconfig.json", "package.json", "package-lock.json", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
+            local project_root = vim.fs.root(bufnr, root_markers)
+            on_dir(project_root)
+        end,
         workspace_required = true,
-        settings = {
-            typescript = {},
-        },
+        settings = ts_and_js_settings,
+    },
+    denols = {
+        -- root_dir = function (bufnr, on_dir)
+        --     local root_markers = { "deno.json", "deno.jsonc" }
+        --     local project_root = vim.fs.root(bufnr, root_markers)
+        --     on_dir(project_root)
+        -- end,
+        root_markers = { "deno.json", "deno.jsonc" },
+        workspace_required = true,
+        settings = ts_and_js_settings,
     },
     biome = {
         workspace_required = true,
-        root_dir = vim.fs.root(0, { "biome.json" }),
     },
+    taplo = {},
 }
+
+local enabled = {}
 
 for server, config in pairs(servers) do
     vim.lsp.config[server] = config
+    if server ~= "*" then
+        table.insert(enabled, server)
+    end
 end
 
-vim.lsp.enable({ "lua_ls", "biome", "denols" })
+vim.lsp.enable(enabled)
