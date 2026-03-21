385d0afd49dd5d44b692388ff65ad94dc87ea9d4
Angju <154499065+timeless-spell@users.noreply.github.com>
2026-03-21 11:21:51 -0400

    Created keymap for enabling colorscheme (tokyonight.nvim)

--- lua/angju/plugins/tokyonight.lua
+++ lua/angju/plugins/tokyonight.lua
@@ -1,3 +1,9 @@
 MiniDeps.now(function ()
-    MiniDeps.add { source = 'folke/tokyonight.nvim' }
+	MiniDeps.add { source = 'folke/tokyonight.nvim' }
+
+	Angju.set_keymap(
+		'<Leader><Space>ct',
+		'<CMD>colorscheme tokyonight-moon<CR>',
+		'Colorscheme:Tokyonight Moon'
+	)
 end)
