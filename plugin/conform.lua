d82be5ef89830b69d381b870adc697b183a878f2
Angju <154499065+timeless-spell@users.noreply.github.com>
2026-05-02 05:22:26 -0400

    Enabled new formatters.

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,13 +1,24 @@
 Angju.later(function ()
 	vim.pack.add { 'https://github.com/stevearc/conform.nvim' }
 
+	local function js_format (bufnr)
+		local deno_root = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc' })
+		if deno_root then
+			return { 'deno_fmt' }
+		else
+			return { 'biome' }
+		end
+	end
+
 	require('conform').setup {
 		formatters_by_ft = {
 			lua = { 'stylua' },
 			html = { 'biome' },
 			css = { 'biome' },
-			javascript = { 'biome', 'deno_fmt', stop_after_first = true },
-			typescript = { 'biome', 'deno_fmt', stop_after_first = true },
+			javascript = js_format,
+			typescript = js_format,
+			json = { 'biome' },
+			toml = { 'taplo' },
 		},
 		format_after_save = {
 			lsp_format = 'fallback',
