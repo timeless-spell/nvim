edca26605e506402b0eab7dc2d447e0c3e0be007
A Timeless Spell <154499065+timeless-spell@users.noreply.github.com>
2026-05-24 14:22:48 -0400

    Fix: keymap descriptions

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,25 +5,27 @@
 
 --- @type ang.Keymap[]
 local keymaps = {
-	{ keys = '<M-d>', func = '<CMD>bnext<CR>', desc = 'Next buffer -->' },
-	{ keys = '<M-a>', func = '<CMD>bprevious<CR>', desc = 'Prev buffer <--' },
+	-- Navigate buffers
+	{ keys = '<M-d>', func = '<CMD>bnext<CR>', desc = 'Next buffer ' },
+	{ keys = '<M-a>', func = '<CMD>bprevious<CR>', desc = 'Prev buffer ' },
 
-	{ keys = '<Esc>', func = '<CMD>nohlsearch<CR>', desc = 'Delete buffer <--' },
-
-	{ keys = '<C-h>', func = '<C-w><C-h>', desc = 'Move focus left' },
-	{ keys = '<C-j>', func = '<C-w><C-j>', desc = 'Move focus down' },
-	{ keys = '<C-k>', func = '<C-w><C-k>', desc = 'Move focus up' },
-	{ keys = '<C-l>', func = '<C-w><C-l>', desc = 'Move focus right' },
+	{ keys = '<Esc>', func = '<CMD>nohlsearch<CR>', desc = 'Clear highlight ❌' },
 
 	{ keys = 'X', func = 'd0', desc = 'Delete all characters before cursor' },
+
+	-- Inspect (:Inspect)
+	{ keys = '<Leader><Space>i', func = '<CMD>Inspect<CR>', desc = 'Inspect' },
 
 	-- Source (:source) files
 	{ keys = '<Leader><Space>sf', func = '<CMD>source %<CR>', desc = 'Source file/buffer' },
 	{ keys = '<Leader><Space>sl', func = '<CMD>.lua<CR>', desc = 'Source line' },
 	{ keys = '<Leader><Space>sc', func = ':"<,">lua<CR>', desc = 'Source selected Lua code', mode = 'x' },
 
-	-- Inspect (:Inspect)
-	{ keys = '<Leader><Space>i', func = '<CMD>Inspect<CR>', desc = 'Inspect' },
+	-- Navigate splits
+	{ keys = '<C-h>', func = '<C-w><C-h>', desc = 'Move focus left ' },
+	{ keys = '<C-j>', func = '<C-w><C-j>', desc = 'Move focus down ' },
+	{ keys = '<C-k>', func = '<C-w><C-k>', desc = 'Move focus up ' },
+	{ keys = '<C-l>', func = '<C-w><C-l>', desc = 'Move focus right ' },
 
 	-- Modify Split Size
 	{ keys = '<C-up>', func = '<CMD>resize -2<CR><CR>', desc = 'Resize split Up' },
