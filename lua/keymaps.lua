2786de05aebb3f2554606cf3712a351f27e945a4
A Timeless Spell <154499065+timeless-spell@users.noreply.github.com>
2026-06-09 02:06:41 -0400

    Created keymap for ':restart' command.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,21 +5,28 @@
 
 --- @type ang.Keymap[]
 local keymaps = {
-	-- Navigate buffers
-	{ keys = '<M-d>', func = '<CMD>bnext<CR>', desc = 'Next buffer ' },
-	{ keys = '<M-a>', func = '<CMD>bprevious<CR>', desc = 'Prev buffer ' },
-
-	{ keys = '<Esc>', func = '<CMD>nohlsearch<CR>', desc = 'Clear highlight ❌' },
+	{ keys = '<Esc>', func = '<CMD>nohlsearch<CR>', desc = 'Clear search highlights' },
 
 	{ keys = 'X', func = 'd0', desc = 'Delete all characters before cursor' },
 
 	-- Inspect (:Inspect)
 	{ keys = '<Leader><Space>i', func = '<CMD>Inspect<CR>', desc = 'Inspect' },
 
+	-- Restart Neovim
+	{ keys = '<Leader><Space>r', func = '<CMD>restart<CR>', desc = 'Restart Neovim' },
+
 	-- Source (:source) files
 	{ keys = '<Leader><Space>sf', func = '<CMD>source %<CR>', desc = 'Source file/buffer' },
 	{ keys = '<Leader><Space>sl', func = '<CMD>.lua<CR>', desc = 'Source line' },
 	{ keys = '<Leader><Space>sc', func = ':"<,">lua<CR>', desc = 'Source selected Lua code', mode = 'x' },
+
+	-- Yank and put + (system) clipboard
+	{ keys = '<Leader><Space>y', func = '"+y', desc = 'Yank to 󰐕 clipboard' },
+	{ keys = '<Leader><Space>p', func = '"+p', desc = 'Put from 󰐕 clipboard' },
+
+	-- Navigate buffers
+	{ keys = '<M-d>', func = '<CMD>bnext<CR>', desc = 'Next buffer ' },
+	{ keys = '<M-a>', func = '<CMD>bprevious<CR>', desc = 'Prev buffer ' },
 
 	-- Navigate splits
 	{ keys = '<C-h>', func = '<C-w><C-h>', desc = 'Move focus left ' },
@@ -32,10 +39,6 @@ local keymaps = {
 	{ keys = '<C-down>', func = '<CMD>resize +2<CR><CR>', desc = 'Resize split Down' },
 	{ keys = '<C-left>', func = '<CMD>vertical resize -2<CR>', desc = 'Resize split Left' },
 	{ keys = '<C-right>', func = '<CMD>vertical resize +2<CR>', desc = 'Resize split Right' },
-
-	-- Yank and put + (system) clipboard
-	{ keys = '<Leader><Space>y', func = '"+y', desc = 'Yank to + clipboard' },
-	{ keys = '<Leader><Space>p', func = '"+p', desc = 'Put from + clipboard' },
 }
 
 Ang.tbl_map (keymaps)
